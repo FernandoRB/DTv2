04e14330a761cedaa3208f2aa5815e0a54296df2
Fernando Ramos <ferbologna08@gmail.com>
2023-08-31 11:22:16 +0200

    four

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'dtv2-51772',
+    appId: '1:896665797460:web:868a96cc365bd328cc3ec1',
+    storageBucket: 'dtv2-51772.appspot.com',
+    apiKey: 'AIzaSyDD_b5NZxuw75RgxNsQZ2jOA6Ymyak12Bs',
+    authDomain: 'dtv2-51772.firebaseapp.com',
+    messagingSenderId: '896665797460',
+    measurementId: 'G-MDVJ1E6FZL',
+  },
+};
